287472dfa4ad3d0561c418fe39c24860906dfe23
yj <yoonjleee@gmail.com>
2022-04-12 10:31:27 -0400

    added 04122022 challenges

---
+++ 04122022.js
@@ -0,0 +1,10 @@
+// Multiply 8kyu
+// This code does not execute properly. Try to figure out why.
+
+multiply = function (a, b){
+    return a * b;
+}
+
+// Another solution:
+
+multiply = (a, b) => a * b;
