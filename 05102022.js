9da6774756d76e68635c3d251ff5a6d2e44ee054
leeyoonj <yoonjleee@gmail.com>
2022-05-10 09:54:22 -0400

    added 05102022 challenges

---
+++ 05102022.js
@@ -0,0 +1,11 @@
+// Convert a string to a number 8kyu
+
+let stringToNumber = function(str){
+    return Number(str);
+  }
+
+// Another solution:
+
+let stringToNum = function(str){
+    return parseInt(str);
+  }
