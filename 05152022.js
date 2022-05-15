1982a3caf51f6812e90be3460faa3883a8e3cb8e
yj <yoonjleee@gmail.com>
2022-05-15 18:24:53 -0400

    added 05152022 challenges

---
+++ 05152022.js
@@ -0,0 +1,13 @@
+// opposite number
+// given an integer or a flaoting point number, find its opposite
+function opposite(number) {
+    return(-number);
+  }
+
+//OR
+const opposite = number => -number;
+
+//OR
+function opposite(number) {
+    return number * (-1);
+}
