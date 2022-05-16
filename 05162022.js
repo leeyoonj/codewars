5f27ddda55ba2da6e56b9f9730f8a69f0ab463d6
yj <yoonjleee@gmail.com>
2022-05-16 11:50:55 -0400

    added 05162022 challenges

---
+++ 05162022.js
@@ -0,0 +1,16 @@
+// Get the Middle Character
+// You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+
+function getMiddle(s)
+{
+  return s.substr(Math.ceil(s.length / 2 - 1), s.length % 2 === 0 ? 2 : 1);
+}
+
+/* Another Solution:
+function getMiddle(s) {
+    var middle = s.length / 2;
+    return (s.length % 2) 
+      ? s.charAt(Math.floor(middle))
+      : s.slice(middle - 1, middle + 1);
+  }
+*/
