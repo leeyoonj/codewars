f423e3f829499e07c92089b81cb4cad3fb11808d
leeyoonj <yoonjleee@gmail.com>
2022-05-19 15:18:52 -0400

    added 05192022 challenges

---
+++ 05192022.js
@@ -0,0 +1,11 @@
+// Returning Strings 8kyu
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+function greet(name){
+    return `Hello, ${name} how are you doing today?`;
+  }
+
+  
+// Another solution:
+
+const greet = name => `Hello, ${name} how are you doing today?`;
