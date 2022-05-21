3553836acf90f852fca0dcac498e6426adfa2e09
leeyoonj <yoonjleee@gmail.com>
2022-05-21 11:33:37 -0400

    added 05212022 challenges

---
+++ 05212022.js
@@ -0,0 +1,14 @@
+// Convert a boolean to a string
+// Implement a function which convert the given boolean value into its string representation.
+
+
+function booleanToString(b){
+    return b.toString();
+  }
+
+
+  // Another solution:
+
+  function booleanToString(b){
+    return b ? 'true' : 'false';
+  }
